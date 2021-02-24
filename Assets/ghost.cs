f21952897d35ed1980786f14d6a41ebc092edeff
thatguy841 <pp>
2021-02-24 13:37:30 -0500

    update 3

---
+++ Assets/ghost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : MonoBehaviour
+{
+
+    public GameObject Ghost;
+    public m script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
